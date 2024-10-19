e642a1cc696d9600fdeef5c53008cfdb5ffd8059
Insha <insharamin34@gmail.com>
2024-10-19 15:42:31 +0530

    let's commit

---
+++ test/integration/InteractionsTest.t.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {Test, console} from "forge-std/Test.sol";
+import {FundMe} from "../../src/FundMe.sol";
+import {DeployFundMe} from "../../script/DeployfundMe.s.sol";
+import {FundFundMe} from "../../script/Interactions.s.sol";
+
+contract FundMeTestIn is Test {
+    FundMe fundMe;
+    address USER = makeAddr("user");
+    uint256 constant SEND_VALUE = 0.1 ether;
+    uint256 constant STARTING_BALANCE = 10 ether;
+    uint256 constant GAS_PRICE = 1;
+
+    function setUp() external {
+        //fundMe = new FundMe(address(1));
+        DeployFundMe deployFundMe = new DeployFundMe();
+        fundMe = deployFundMe.run();
+        vm.deal(USER, STARTING_BALANCE);
+    }
+    function testUserCanFundInteractions() public {
+        FundFundMe fundFundMe = new FundFundMe();
+        fundFundMe.fundFundMe(address(fundMe));
+
+        address funder = fundMe.getFunder(0);
+        assertEq(funder, USER);
+    }
+
+}
